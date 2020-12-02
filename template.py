891954e8bd695f7479a0302804205b6f31b2f12a
Mathis Dedial <mathis@dedial.net>
2020-12-02 10:32:48 +0100

    Template

---
+++ template.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+import os.path
+
+
+INPUT_FILE = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def part_01():
+    with open(INPUT_FILE, 'r') as input_file:
+        pass
+
+
+if __name__ == '__main__':
+    part_01()
